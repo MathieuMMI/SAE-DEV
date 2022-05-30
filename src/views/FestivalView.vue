79bce27d03e3f6b9bc9615d423326d05b9e760ff
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-05-30 11:17:54 +0200

    festival

--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -1,6 +1,14 @@
 <template>
   <main>
-    <h1 class="text-2xl">Festival</h1>
+    <img src="public\img\festival.jpg" />
+    <h2 class="mt-5 text-center text-base font-semibold text-black">Notre festival</h2>
+    <p class="mt-3 ml-3 mb-5 mr-2 text-left text-sm text-black">
+      Notre festival....Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+      takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+      tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    </p>
   </main>
 </template>
 
