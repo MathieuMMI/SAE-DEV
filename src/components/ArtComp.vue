6bdd8c690c249b637e9739ea6f934ad5bc0f1343
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-05-30 18:45:48 +0200

    responsive_artiste_compo

--- src/components/ArtComp.vue
+++ src/components/ArtComp.vue
@@ -1,22 +1,11 @@
 <template>
-  <div class="relative h-[200px] w-[105px]">
-    <div class="">
-      <div class="h-[150px] w-[105px]">
-        <div class=
-          "relative
-          left-[-0.5px]
-          top-[-0.5px]
-          h-[154px]
-          w-[105px]
-          rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[35px] rounded-br-[35px]
-          bg-slate-800">
-          <p class="h-4 text-center p-0.5 text-base font-semibold text-amber-400">Vincent Niclo</p>
-        </div>
-      </div>
-      <img src="public\img\composant_art.jpg" class="absolute left-[-0.5px] top-[26.5px] h-[127px] w-[105px] rounded-bl-[35px] rounded-br-[35px] object-cover"/>
+  <figure class="flex flex-col">
+    <div>
+      <h4 class="z-10 m-auto w-max rounded-2xl bg-slate-900 p-1 text-base font-semibold text-amber-400">Vincent Niclo</h4>
+      <img src="public\img\composant_art.jpg" class="w-full" alt="Image du festival" />
+      <figcaption class="rounded-b-lg">
+        <p class="z-10 m-auto rounded-2xl bg-slate-900 p-1 text-center text-sm text-amber-400">En savoir plus</p>
+      </figcaption>
     </div>
-      <div class="h-[30px] w-[100px] rounded-[20px] bg-stone-900">
-      <p class="text-center text-sm text-amber-400 pt-1">En savoir plus</p>
-      </div>
-  </div>
+  </figure>
 </template>
--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,15 +1,24 @@
 <template>
   <main>
     <img src="public\img\accueil.jpg" />
-    <h2 class="mt-3 ml-3 text-left text-base font-semibold text-black">Programmation</h2>
-    <div class="mt-5 flex justify-center">
-      <prog-comp />
-      <prog-comp class="relative top-44 md:top-56" />
+    <div class="">
+      <h2 class="mt-3 ml-3 text-left text-base font-semibold text-black">Programmation</h2>
+      <div class="mb-5 ml-3 mr-3 mt-5 flex justify-center md:ml-8 md:mr-8">
+        <prog-comp />
+        <prog-comp class="mt-44 md:mt-72" />
+        <prog-comp class="hidden lg:flex"/>
+        <prog-comp class="hidden mt-72 xl:flex"/>
+      </div>
     </div>
-    <h2 class="mt-52 md:mt-40 ml-3 text-left text-base font-semibold text-black">Artistes</h2>
-    <div class="mt-5 flex justify-center gap-12">
-      <art-comp />
-      <art-comp />
+    <div class="mb-28 ml-3 mr-3">
+      <h2 class="mt-66 ml-3 text-left text-base font-semibold text-black">Artistes</h2>
+      <div class="mt-5 flex justify-center gap-3">
+        <art-comp />
+        <art-comp />
+        <art-comp class="hidden md:flex"/>
+        <art-comp class="hidden lg:flex"/>
+        <art-comp class="hidden xl:flex"/>
+      </div>
     </div>
   </main>
 </template>
