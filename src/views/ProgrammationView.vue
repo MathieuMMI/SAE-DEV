e94e63dcf42f43ad5ba86aaae2631ca03e525275
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-05-30 19:01:08 +0200

    programmation

--- src/views/ProgrammationView.vue
+++ src/views/ProgrammationView.vue
@@ -1,9 +1,33 @@
 <template>
-  <main>
-    <h1 class="text-2xl">Programmation</h1>
+  <main class="mb-36">
+    <h2 class="mt-5 mb-7 text-center text-base font-semibold text-black">Programmation</h2>
+    <div class="flex justify-center gap-3 md:ml-8 md:mr-8">
+      <prog-comp />
+      <prog-comp class="mt-44 md:mt-72 xl:hidden" />
+      <prog-comp class="hidden xl:flex" />
+      <prog-comp class="hidden xl:flex" />
+      <prog-comp class="hidden xl:flex" />
+    </div>
+    <div class="mb-5 ml-3 mr-3 mt-5 flex justify-center gap-3 md:ml-8 md:mr-8">
+      <prog-comp />
+      <prog-comp class="mt-44 md:mt-72 xl:hidden" />
+      <prog-comp class="hidden xl:flex" />
+      <prog-comp class="hidden xl:flex" />
+      <prog-comp class="hidden xl:flex" />
+    </div>
+    <div class="mb-5 ml-3 mr-3 mt-5 flex justify-center gap-3 md:ml-8 md:mr-8">
+      <prog-comp />
+      <prog-comp class="mt-44 md:mt-72 xl:hidden" />
+      <prog-comp class="hidden xl:flex" />
+      <prog-comp class="hidden xl:flex" />
+      <prog-comp class="hidden xl:flex" />
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import ProgComp from "../components/ProgComp.vue";
+export default {
+  components: { ProgComp },
+};
 </script>
