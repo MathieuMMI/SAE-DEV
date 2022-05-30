184fe5ab8ad7c35a54b63a88326f4339fed6f0f0
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-05-30 13:26:55 +0200

    corrections_festival_accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -2,7 +2,7 @@
   <main>
     <img src="public\img\accueil.jpg" />
     <h2 class="mt-3 ml-3 text-left text-base font-semibold text-black">Programmation</h2>
-    <div class="mt-5 flex">
+    <div class="mt-5 flex justify-center">
       <prog-comp />
       <prog-comp class="relative top-44" />
     </div>
--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -2,12 +2,19 @@
   <main>
     <img src="public\img\festival.jpg" />
     <h2 class="mt-5 text-center text-base font-semibold text-black">Notre festival</h2>
-    <p class="mt-3 ml-3 mb-5 mr-2 text-left text-sm text-black">
+    <p class="mt-3 ml-3 mr-2 text-left text-sm text-black">
       Notre festival....Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
       magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
       takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
       tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
       Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    </p>
+    <p class="mt-3 ml-3 mb-12 mr-2 text-left text-sm text-black">
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+      est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+      labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     </p>
   </main>
 </template>
