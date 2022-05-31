449ee32c47322faa3cd1453c7812889d811db0de
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-05-31 08:23:28 +0200

    formulaire_de_contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,13 @@
 <template>
   <main>
-    <h1 class="text-2xl">Contact</h1>
+    <h2 class="mb-5 mt-5 text-center text-base font-semibold text-black">Formulaire de contact</h2>
+    <div class="flex flex-col gap-5 mb-[100%]">
+      <input class="m-auto w-[40%] rounded-[200px] border border-amber-600 bg-white" type="text" placeholder="Nom" />
+      <input class="m-auto w-[40%] rounded-[200px] border border-amber-600 bg-white" type="text" placeholder="Prénom" />
+      <input class="m-auto w-[40%] rounded-[200px] border border-amber-600 bg-white" type="text" placeholder="Email" />
+      <input class="m-auto w-[40%] rounded-[200px] border border-amber-600 bg-white" type="text" placeholder="Téléphone (optionnel)" />
+      <input class="m-auto w-[40%] h-56 rounded-[50px] bg-white border border-amber-600" type="text" placeholder="Message" />
+    </div>
   </main>
 </template>
 
