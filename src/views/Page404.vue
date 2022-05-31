c4d760c4aaf9b8e827a383a470941f37b1795c84
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-05-31 11:14:36 +0200

    page_404

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,3 +1,22 @@
 <template>
-    <h2 class="text-2xl font-semibold">HHHH</h2>
+  <h2 class="mt-5 text-center text-2xl font-semibold">ERREUR 404 - Page introuvable</h2>
+  <div class="mb-[100%] mt-7 justify-center md:flex md:gap-3.5">
+    <router-link to="/">
+      <h3 class="mb-2 text-left text-2xl font-semibold text-black">Accueil</h3>
+    </router-link>
+    <router-link to="./views/programmation">
+      <h3 class="mb-2 text-left text-2xl font-semibold text-black">Programmation</h3>
+    </router-link>
+    <router-link to="./views/artistes">
+      <h3 class="mb-2 text-left text-2xl font-semibold text-black">Artistes</h3>
+    </router-link>
+    <router-link to="./views/festival">
+      <h3 class="mb-2 text-left text-2xl font-semibold text-black">Festival</h3>
+    </router-link>
+    <router-link to="./views/contact">
+      <h3 class="mb-2 text-left text-2xl font-semibold text-black">Formulaire de contact</h3>
+    </router-link>
+  </div>
 </template>
+
+
