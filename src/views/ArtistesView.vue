df3e866889a65c3cd1285ec208df2f19da727ef4
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-05-31 08:28:36 +0200

    artistes

--- src/views/ArtistesView.vue
+++ src/views/ArtistesView.vue
@@ -1,9 +1,33 @@
 <template>
-  <main>
-    <h1 class="text-2xl">Artistes</h1>
+   <main class="mb-36">
+    <h2 class="mt-5 mb-7 text-center text-base font-semibold text-black">Artistes</h2>
+    <div class="flex justify-center gap-3 md:ml-8 md:mr-8">
+      <art-comp />
+      <art-comp />
+      <art-comp class="hidden md:flex" />
+      <art-comp class="hidden lg:flex" />
+      <art-comp class="hidden xl:flex" />
+    </div>
+    <div class="mb-5 ml-3 mr-3 mt-5 flex justify-center gap-3 md:ml-8 md:mr-8">
+      <art-comp />
+      <art-comp />
+      <art-comp class="hidden md:flex" />
+      <art-comp class="hidden lg:flex" />
+      <art-comp class="hidden xl:flex" />
+    </div>
+    <div class="mb-5 ml-3 mr-3 mt-5 flex justify-center gap-3 md:ml-8 md:mr-8">
+      <art-comp />
+      <art-comp />
+      <art-comp class="hidden md:flex" />
+      <art-comp class="hidden lg:flex" />
+      <art-comp class="hidden xl:flex" />
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import ArtComp from "../components/ArtComp.vue";
+export default {
+  components: { ArtComp, },
+};
 </script>
