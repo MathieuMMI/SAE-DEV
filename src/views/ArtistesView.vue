0c567a496596c0a05687bccfabb3e784428e0cf1
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-05-31 13:52:34 +0200

    liens_redirection

--- src/views/ArtistesView.vue
+++ src/views/ArtistesView.vue
@@ -2,25 +2,25 @@
   <main class="mb-36">
     <h2 class="mt-5 mb-7 text-center text-2xl font-semibold text-black dark:text-white">Artistes</h2>
     <div class="flex justify-center gap-3 md:ml-8 md:mr-8">
-      <art-comp />
-      <art-comp />
-      <art-comp class="hidden md:flex" />
-      <art-comp class="hidden lg:flex" />
-      <art-comp class="hidden xl:flex" />
+      <router-link to="./views/Artiste"><art-comp /></router-link>
+      <router-link to="./views/Artiste"><art-comp /></router-link>
+      <router-link to="./views/Artiste"><art-comp class="hidden md:flex" /></router-link>
+      <router-link to="./views/Artiste"><art-comp class="hidden lg:flex" /></router-link>
+      <router-link to="./views/Artiste"><art-comp class="hidden xl:flex" /></router-link>
     </div>
     <div class="mb-5 ml-3 mr-3 mt-5 flex justify-center gap-3 md:ml-8 md:mr-8">
-      <art-comp />
-      <art-comp />
-      <art-comp class="hidden md:flex" />
-      <art-comp class="hidden lg:flex" />
-      <art-comp class="hidden xl:flex" />
+      <router-link to="./views/Artiste"><art-comp /></router-link>
+      <router-link to="./views/Artiste"><art-comp /></router-link>
+      <router-link to="./views/Artiste"><art-comp class="hidden md:flex" /></router-link>
+      <router-link to="./views/Artiste"><art-comp class="hidden lg:flex" /></router-link>
+      <router-link to="./views/Artiste"><art-comp class="hidden xl:flex" /></router-link>
     </div>
     <div class="mb-5 ml-3 mr-3 mt-5 flex justify-center gap-3 md:ml-8 md:mr-8">
-      <art-comp />
-      <art-comp />
-      <art-comp class="hidden md:flex" />
-      <art-comp class="hidden lg:flex" />
-      <art-comp class="hidden xl:flex" />
+      <router-link to="./views/Artiste"><art-comp /></router-link>
+      <router-link to="./views/Artiste"><art-comp /></router-link>
+      <router-link to="./views/Artiste"><art-comp class="hidden md:flex" /></router-link>
+      <router-link to="./views/Artiste"><art-comp class="hidden lg:flex" /></router-link>
+      <router-link to="./views/Artiste"><art-comp class="hidden xl:flex" /></router-link>
     </div>
   </main>
 </template>
--- src/views/ProgrammationView.vue
+++ src/views/ProgrammationView.vue
@@ -1,25 +1,25 @@
 <template>
   <h2 class="mt-5 mb-7 text-center text-2xl font-semibold text-black">Programmation</h2>
   <div class="flex justify-center gap-3 md:ml-8 md:mr-8">
-    <prog-comp />
-    <prog-comp class="mt-44 md:mt-72 xl:hidden" />
-    <prog-comp class="hidden xl:flex" />
-    <prog-comp class="hidden xl:flex" />
-    <prog-comp class="hidden xl:flex" />
+    <router-link to="./views/Concert"><prog-comp /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="mt-44 md:mt-72 xl:hidden" /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="hidden xl:flex" /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="hidden xl:flex" /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="hidden xl:flex" /></router-link>
   </div>
   <div class="mb-5 ml-3 mr-3 mt-5 flex justify-center gap-3 md:ml-8 md:mr-8">
-    <prog-comp />
-    <prog-comp class="mt-44 md:mt-72 xl:hidden" />
-    <prog-comp class="hidden xl:flex" />
-    <prog-comp class="hidden xl:flex" />
-    <prog-comp class="hidden xl:flex" />
+    <router-link to="./views/Concert"><prog-comp /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="mt-44 md:mt-72 xl:hidden" /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="hidden xl:flex" /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="hidden xl:flex" /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="hidden xl:flex" /></router-link>
   </div>
   <div class="mb-28 ml-3 mr-3 mt-5 flex justify-center gap-3 md:ml-8 md:mr-8">
-    <prog-comp />
-    <prog-comp class="mt-44 md:mt-72 xl:hidden" />
-    <prog-comp class="hidden xl:flex" />
-    <prog-comp class="hidden xl:flex" />
-    <prog-comp class="hidden xl:flex" />
+    <router-link to="./views/Concert"><prog-comp /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="mt-44 md:mt-72 xl:hidden" /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="hidden xl:flex" /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="hidden xl:flex" /></router-link>
+    <router-link to="./views/Concert"><prog-comp class="hidden xl:flex" /></router-link>
   </div>
 </template>
 
