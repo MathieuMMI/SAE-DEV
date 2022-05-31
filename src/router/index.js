cf31bced1fe7d1660da4a122465f0e0f5d0f2b47
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-05-31 09:03:27 +0200

    import_404

--- src/router/index.js
+++ src/router/index.js
@@ -7,20 +7,20 @@ import ContactView from '../views/ContactView.vue'
 import ConcertView from '../views/ConcertView.vue'
 import ArtisteView from '../views/ArtisteView.vue'
 import MentionsView from '../views/MentionsView.vue'
+import Page404 from '../views/Page404.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/',              name: 'Accueil',        component: AccueilView },
-    { path: '/programmation', name: 'Programmation',  component: ProgrammationView },
-    { path: '/artistes',      name: 'Artistes',       component: ArtistesView },
-    { path: '/festival',      name: 'Festival',       component: FestivalView },
-    { path: '/contact',       name: 'Contact',        component: ContactView },
-    { path: '/concert',       name: 'Concert',        component: ConcertView },
-    { path: '/artiste',       name: 'Artiste',        component: ArtisteView },
-    { path: '/mentions',      name: 'Mentions',       component: MentionsView },
-    
-    // ici les autre routes
+    { path: '/',                  name: 'Accueil',        component: AccueilView },
+    { path: '/programmation',     name: 'Programmation',  component: ProgrammationView },
+    { path: '/artistes',          name: 'Artistes',       component: ArtistesView },
+    { path: '/festival',          name: 'Festival',       component: FestivalView },
+    { path: '/contact',           name: 'Contact',        component: ContactView },
+    { path: '/concert',           name: 'Concert',        component: ConcertView },
+    { path: '/artiste',           name: 'Artiste',        component: ArtisteView },
+    { path: '/mentions',          name: 'Mentions',       component: MentionsView },
+    { path: '/:pathMatch(.*)*',   name: 'Page404',        component: Page404 },
   ]
 })
 
